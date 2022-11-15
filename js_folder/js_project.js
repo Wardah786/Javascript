5c30e9726fc1ee672e44b77102ee507e5559e9ca
Wardah <waroumoor@gmail.com>
2022-11-15 11:46:46 +0400

    My first commit

---
+++ js_folder/js_project.js
@@ -0,0 +1,9 @@
+let addressNumber, addressStreet, Country;
+
+addressNumber = "47";
+addressStreet = "royal road Tyack";
+country = "Mauritius";
+
+let generalAddress = addressNumber + " " + addressStreet + " " + country;
+console.log(generalAddress)
+
